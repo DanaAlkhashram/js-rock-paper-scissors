b98898b2cbfc1e97a7313790a3734e073c924628
Dana <danalkhashram@gmail.com>
2025-06-29 11:10:08 +0300

    create play and getComputerChoice

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*-------------------------------- Constants --------------------------------*/
 const choices = ['rock', 'paper', 'scissors'];
-// smiller to node list
+// smiller to node list and the better to use is object
 
 /*-------------------------------- Variables --------------------------------*/
 let playerChoice;
@@ -15,6 +15,31 @@ const scissorsbtnElement = document.querySelector('#scissors');
 
 /*-------------------------------- Functions --------------------------------*/
 
+function getComputerChoice(){
+    // generarete a random number from 0-2
+    const randomIndex = Math.floor(Math.random()* choices.length); //or put 3 instead of lenght
 
+    // slect the item form array
+    return choices[randomIndex]; 
+}
+
+// intialize game state
+function play(){
+    computerChoice = getComputerChoice();
+    console.log("computerChoice",computerChoice);
+    computerChoice = getComputerChoice();
+    console.log("computerChoice",computerChoice);
+
+
+    // after updating state, render to html
+    render();
+}
+
+play();
+
+// updates our UI/html directly
+function render(){
+
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
