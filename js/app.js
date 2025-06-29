36be10d3a4cc0663263c7463e3cc95fe656fed88
Dana <danalkhashram@gmail.com>
2025-06-29 10:45:51 +0300

    cache buttons' and creat state variables

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log('work');
+/*-------------------------------- Constants --------------------------------*/
+const choices = ['rock', 'paper', 'scissors'];
+// smiller to node list
+
+/*-------------------------------- Variables --------------------------------*/
+let playerChoice;
+let computerChoice; 
+let msg;
+
+/*------------------------ Cached Element References ------------------------*/
+const rockbtnElement = document.querySelector('#rock');
+const paperbtnElement = document.querySelector('#paper');
+const scissorsbtnElement = document.querySelector('#scissors');
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
